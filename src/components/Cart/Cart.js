60e5580d858f3b406ae1e0c993f430547b27a07f
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-09-25 14:46:22 +0600

    search and filter out products from search text done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,20 +7,26 @@ const Cart = (props) => {
       // const total = cart.reduce((previous, product) => previous + product.price, 0);
       
       const{cart} = props;
+      // console.log(cart);
+      let totalQuantity = 0;
       let total = 0;
       for(const product of cart){
-            total = total + product.price;
+            if(!product.quantity){
+                  product.quantity = 1;
+            }
+            total = total + product.price * product.quantity;
+            totalQuantity = totalQuantity + product.quantity;
       }
 
-      const shipping = 50;
-      const tax = (total + shipping) * .10;
+      const shipping = total > 0 ? 50 : 0;
+      const tax = (total + shipping) * .075;
       const grandTotal = total + shipping + tax;
 
       return (
             <div className="cart__div">
                    <div id="cart_div">
                    <h3>Order <span id="cart_title">Summary</span></h3>
-                   <h5>Items Ordered: <span>{props.cart.length}</span></h5>
+                   <h5>Items Ordered: <span>{totalQuantity}</span></h5>
                    <hr />
                         <p>Total: <span>${total.toFixed(2)}</span></p>
                         <hr />
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,29 +1,83 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
+import {addToDb, getStoredCart} from '../../utilities/fakedb'
 import './Shop.css';
 
 const Shop = () => {
       const [products, setProducts] = useState([]);
       const [cart, setCart] = useState([]);
+      const [displayProducts, setDisplayProducts] = useState([]);
 
       useEffect( () => {
+            // console.log('product api called')
             fetch('./products.JSON')
             .then(res => res.json())
-            .then(data => setProducts(data));
-      }, [])
+            .then(data =>{ 
+                  setProducts(data);
+                  
+                  //display initial product data
+                  setDisplayProducts(data);
+            // console.log('product recieved');
+
+            });
+      }, []);
+
+      useEffect(()=>{
+            // console.log('local storage called');
+            const savedCart = getStoredCart();
+            const storedCart = [];
+            // console.log(savedCart);
+           if(products.length){
+            for (const key in savedCart){
+                  // console.log(key);
+                  // console.log(products);
+                  // console.log(savedCart[key]);
+                  const addedProduct = products.find( product => product.key === key);
+                  if(addedProduct){
+                        const quantity = savedCart[key];
+                        addedProduct.quantity = quantity;
+                        // console.log(addedProduct);
+                        storedCart.push(addedProduct);
+
+                  }
+                  // console.log(key, addedProduct);
+            } 
+            setCart(storedCart);
+           }
+      }, [products])
 
       const handleAddToCart =(product) =>{
             // console.log(product);
             const newCart = [...cart, product]; //spread operation
             setCart(newCart);
+            //save to local storage
+            addToDb(product.key);
+      }
+      //search product
+      const handleSearch = event =>{
+            const searchText = event.target.value;
+            //filter out products by given search text
+            const matchedProducts = products.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase()));
+
+            setDisplayProducts(matchedProducts);
+            // console.log(matchedProducts.length);
       }
 
       return (
-            <div className="shop__container">
-                  <div className="product__container">
-                        {
-                              products.map(product => <Product 
+            //using empty tag / fragment alt to div
+            <>
+                  <div className="search__container">
+                        <input
+                        type="text"
+                        onChange={ handleSearch }
+                        placeholder="Search Products"/>
+                  </div>
+
+                  <div className="shop__container">
+                        <div className="product__container">
+                              {
+                              displayProducts.map(product => <Product 
                               key = {product.key}
                               product = {product}
                               handleAddToCart = {handleAddToCart}
@@ -31,10 +85,11 @@ const Shop = () => {
                               </Product>)
                         }
                   </div>
-                  <div className="cart__container">
-                       <Cart cart={cart}></Cart>
+                        <div className="cart__container">
+                               <Cart cart={cart}></Cart>
+                        </div>
                   </div>
-            </div>
+            </>
       );
 };
 
