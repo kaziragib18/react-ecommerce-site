b5c2b9e8ebe312802ba227aa0c082f44fd515b42
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-10-26 18:02:50 +0600

    Saved orders in orders  data collection in mongodb

--- src/components/Shipping/Shipping.js
+++ src/components/Shipping/Shipping.js
@@ -1,15 +1,34 @@
-import userEvent from '@testing-library/user-event';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import useAuth from '../../hooks/useAuth';
+import { clearTheCart, getStoredCart } from '../../utilities/fakedb'
 import './Shipping.css'
 
 const Shipping = () => {
-      const { register, handleSubmit, formState: { errors } } = useForm();
-      const {user} = useAuth();
+      const { register, handleSubmit, reset, formState: { errors } } = useForm();
+      const { user } = useAuth();
 
-      const onSubmit = data =>{ 
-            console.log(data)
+      const onSubmit = data => {
+            const savedCart = getStoredCart();
+            data.order = savedCart;
+            console.log(data);
+
+            fetch('http://localhost:5000/orders', {
+                  method: 'POST',
+                  headers: {
+                        'content-type': 'application/json'
+                  },
+                  body: JSON.stringify(data)
+            })
+                  .then(res => res.json())
+                  .then(result => {
+                        // console.log(result);
+                        if (result.insertedId) {
+                              alert('Order Processed Successfully')
+                              clearTheCart();
+                              reset();
+                        }
+                  })
       };
 
       return (
@@ -21,9 +40,9 @@ const Shipping = () => {
                         <input placeholder="Your email" defaultValue={user.email} {...register("email", { required: true })} />
 
                         {errors.email && <span className="error">This field is required</span>}
-                        <input placeholder="Phone number" defaultValue="" {...register("phone number",  { required: true })} />
-                        <input placeholder="Address" defaultValue="" {...register("address",  { required: true })} />
-                        <input placeholder="City" defaultValue="" {...register("city",  { required: true })} />
+                        <input placeholder="Phone number" defaultValue="" {...register("phone number", { required: true })} />
+                        <input placeholder="Address" defaultValue="" {...register("address", { required: true })} />
+                        <input placeholder="City" defaultValue="" {...register("city", { required: true })} />
                         <button className="button">Submit</button>
                   </form>
             </div>
